be5821fa165960dc6717d29319d24bba987a1adb
Cao Qian <talexcao@gmail.com>
2018-01-26 14:18:33 -0800

    adjusting radius

--- src/components/Home.js
+++ src/components/Home.js
@@ -109,9 +109,10 @@ export class Home extends React.Component {
                         <WrappedAroundMap
                             googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
                             loadingElement={<div style={{ height: `100%` }} />}
-                            containerElement={<div style={{ height: `400px` }} />}
+                            containerElement={<div style={{ height: `600px` }} />}
                             mapElement={<div style={{ height: `100%` }} />}
                             posts={this.state.posts}
+                            loadNearbyPosts={this.loadNearbyPosts}
                         />
                     </TabPane>
                 </Tabs>
